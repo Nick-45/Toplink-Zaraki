79ce153e316ed8f769f54180d119d395430480bf
NICKSON JAMES <obarenickson2@gmail.com>
2025-11-16 23:18:20 +0300

    Create encryption.js

---
+++ utils/encryption.js
@@ -0,0 +1,16 @@
+// utils/encryption.js
+const crypto = require('crypto');
+
+const encrypt = (text) => {
+    const cipher = crypto.createCipher('aes-256-cbc', process.env.ENCRYPTION_KEY);
+    let encrypted = cipher.update(text, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return encrypted;
+};
+
+const decrypt = (encryptedText) => {
+    const decipher = crypto.createDecipher('aes-256-cbc', process.env.ENCRYPTION_KEY);
+    let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+};
